c92546a1840f01651c0efa62c788b6d2ec5b2f03
asuzukosi <keloasuzu@yahoo.com>
2023-08-10 03:44:56 +0100

    feat: add dockerization for SOA application

--- Shipping-Service/src/main.py
+++ Shipping-Service/src/main.py
@@ -8,11 +8,13 @@ import threading
 import uvicorn
 from rest_service import app
 
+
 def serveGRPC():
     print("Starting GRPC Server")
     port = "50051"
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    shipping_pb2_grpc.add_ShippingServicer_to_server(ShippingGRPCService(), server)
+    shipping_pb2_grpc.add_ShippingServicer_to_server(ShippingGRPCService(),
+                                                     server)
     server.add_insecure_port("[::]:" + port)
     server.start()
     print("GRPC Server started, listening on ", port)
@@ -26,10 +28,10 @@ def serveHTTP():
     print("HTTP Server started, listening on ", port)
     uvicorn.run(app, host=host, port=port)
 
+
 if __name__ == "__main__":
     logging.basicConfig()
     # start grpc server in seperate thread
-    serveGRPC()
-    # threading.Thread(target=serveGRPC, name="grpc-server").start()
-    # threading.Thread(target=serveHTTP, name="rest-server").start()
-    
+    # serveGRPC()
+    threading.Thread(target=serveGRPC, name="grpc-server").start()
+    threading.Thread(target=serveHTTP, name="rest-server").start()
