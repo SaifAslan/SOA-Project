9a99bced94685cbfbd7bb8b0615bd1d6391f0490
buketfildisi <buketfildisi@gmail.com>
2023-08-12 15:49:00 +0100

    product serv

---
+++ Products-Serivce/ProductWebApi/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductWebApi.Models;
+
+namespace ProductWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext _productDbContext;
+        public ProductController(ProductDbContext productDbContext) 
+        { 
+            _productDbContext = productDbContext;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return _productDbContext.Products;
+        }
+        [HttpGet("{productId:int}")]
+        public async Task<ActionResult<Product>> GetById(int productId)
+        {
+            var product = await _productDbContext.Products.FindAsync(productId);
+            return product;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(Product product)
+        {
+            await _productDbContext.Products.AddAsync(product);
+            //saves db changes
+            await _productDbContext.SaveChangesAsync();
+            return Ok();
+
+        }
+        //Updates existing record
+        //excepts product object as a parameter
+        [HttpPut]
+        public async Task<ActionResult> Update(Product product)
+        {
+            _productDbContext.Products.Update(product);
+            await _productDbContext.SaveChangesAsync();
+            return Ok(product);
+        }
+        [HttpDelete("{productId:int}")]
+        public async Task<ActionResult> Delete(int productId)
+        {
+            var product = await _productDbContext.Products.FindAsync(productId);
+            _productDbContext.Products.Remove(product);
+            await _productDbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+
+    }
+}
---
+++ Products-Serivce/ProductWebApi/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductWebApi.Models
+{
+    [Table("product",Schema ="dbo")]
+    public class Product
+    {
+              
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        [Column("ProductId")]
+        public int ProductId { get; set; }
+        [Column("ProductName")]
+        public string ProductName { get; set; }
+        [Column("ProductDescription")]
+        public string ProductDescription { get; set; }
+
+        [Column("Category")]
+        public string Category { get; set;}
+
+        [Column("Price")]
+        public double Price { get; set; }
+        [Column("CreatedDate")]
+        public DateTime CreatedDate { get; set; }
+        [Column("UpdatedDate")]
+        public DateTime UpdatedDate { get;  set; }
+
+    }
+}
